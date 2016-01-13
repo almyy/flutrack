7e84ac6d0532f4e296a0b03f81677076fcd95b5e
Almyy <martalm@stud.ntnu.no>
2016-01-13 17:12:28 +0100

    Heatmap up and running

--- www/js/Controllers/flutrackCtrl.js
+++ www/js/Controllers/flutrackCtrl.js
@@ -4,9 +4,27 @@
 angular.module('starter.controllers', ['starter.services'])
 
 .controller('FlutrackCtrl', function ($scope, HttpService) {
-        HttpService.get('/api', '?s=feverANDcough').then(function(res) {
-            $scope.text = res;
+		var data = [];
+        HttpService.get('/api', '/results.json').then(function(res) {
+            for(tweet in res.data) {
+            	data.push(new google.maps.LatLng(res.data[tweet].latitude, res.data[tweet].longitude));
+        	}
+        	var mapOptions = {
+        		zoom: 4,
+        		center: new google.maps.LatLng(40, -98),
+        		mapTypeId: google.maps.MapTypeId.SATELLITE
+    		};
+			var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+			var heatmap = new google.maps.visualization.HeatmapLayer({
+				data: data,
+				map: map
+			});
+			heatmap.set('radius', 20);
+			heatmap.set('opacity', 1);
+			$scope.map = map;
         }, function (error) {
             $scope.text = error;
-        })
+        });
+
+		
     });
